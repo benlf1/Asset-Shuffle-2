6b1736b4057adf9dc1c1be8c22074be750919cb4
benlf1 <54038475+benlf1@users.noreply.github.com>
2021-04-05 15:33:31 -0700

    idk what happened

---
+++ Asset-Shuffle-2/Assets/Scripts/Player Scripts/BulletScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    public float moveSpeed = 1;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 velocity = transform.forward * moveSpeed * Time.deltaTime;
+        transform.position += velocity;
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
--- Asset-Shuffle-2/Assets/Scripts/Player Scripts/PlayerController.cs
+++ Asset-Shuffle-2/Assets/Scripts/Player Scripts/PlayerController.cs
@@ -10,6 +10,10 @@ public class PlayerController : Controller
     public PlayerHost host;
 
     [SerializeField] private Vector2 currDirection;
+    private float shootTime;
+    public float shootCooldown;
+
+    public GameObject bullet;
 
     #region Update
     // Iterate each frame to check for player input
@@ -17,6 +21,7 @@ public class PlayerController : Controller
     {
         // Check for any play input
         GetInput();
+        Shoot();
 
         // Rotate Player to Face towards mouse
         host.Face((Vector3)currDirection);
@@ -35,6 +40,15 @@ public class PlayerController : Controller
     {
         Vector2 mousePos = Input.mousePosition; // Returns coordinates in the Screen Coordinate System
         currDirection = Camera.main.ScreenToWorldPoint(mousePos) - transform.position; // Converts from Screen position to World Position relative to the player's position
+    }
+
+    private void Shoot()
+    {
+        if (Input.GetButton("Fire1") && Time.time >= shootTime)
+        {
+            GameObject b = Instantiate(bullet, transform.position + transform.forward * 1, transform.rotation);
+            shootTime = Time.time + shootCooldown;
+        }
     }
     #endregion
 }
